b23a82f54dea0e20925435d08ead8fa45000ae82
Malav Naik <malavnaik12@gmail.com>
2022-04-19 13:02:05 -0400

    Measurement Function - Ref. Trajectories
    Created a function that adds Gaussian white noise to the reference trajectories of the agents in the formation.

---
+++ rangeMeasurementAddedNoise.m
@@ -0,0 +1,10 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This function takes the reference trajectories for Agents in the
+% formation and adds white Gaussian noise to simulate the measured position
+% information of the agents. The simulated measurements of the agents are
+% then used for IMU/GPS fusion among the Agents within the formation.
+% 
+% Author: Malav Naik
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [measurements] = rangeMeasurementAddedNoise(refTraj)
