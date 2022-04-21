bc48027e0336750b0fbddf9d697fc1ef3f721f30
Malav Naik <malavnaik12@gmail.com>
2022-04-21 17:10:26 -0400

    Update compute_pos.m
    Updated the measurement covariance calculation.

--- compute_pos.m
+++ compute_pos.m
@@ -10,12 +10,11 @@
 function [pos_meas_n, meas_Cov_n] = compute_pos(ekf, rel_vec, rel_Cov)
 
 % Zhiyao's function: stateComputeAgent1()
-[agent1State_est_est, agent1Pos_est_est_Cov] = stateComputeAgent1(ekf,prev_pos_1, prev_vel_1, dt, curr_pos_1, drop_percent, est_est_flag);
+agent1State_est_est = stateComputeAgent1(ekf,prev_pos_1, prev_vel_1, dt, curr_pos_1, drop_percent, est_est_flag);
 
 agent1Pos_est_est = agent1State_est_est(1:3,1);
 
 pos_meas_n = agent1Pos_est_est + rel_vec;
 
+agent1Pos_est_est_Cov = ekf.StateCovariance;
 meas_Cov_n = rel_Cov + agent1Pos_est_est_Cov; 
-            % Not sure about agent1Pos_est_est_Cov above, we can discuss on
-            % Saturday
