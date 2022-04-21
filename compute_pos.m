5e3cb9ffe22d8dd2857deba0bad5c25eb4a04555
Malav Naik <malavnaik12@gmail.com>
2022-04-21 17:03:55 -0400

    Updated compute_pos() function
    I am a little unsure how to characterize the covariance of the state estimate (I can't quite remember if there is a covariance matrix associated with the state estimate). If there is no covariance associated with the state estimate, the covariance of the position of Agent n is just the covariance of the range measurement.

--- compute_pos.m
+++ compute_pos.m
@@ -8,3 +8,14 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function [pos_meas_n, meas_Cov_n] = compute_pos(ekf, rel_vec, rel_Cov)
+
+% Zhiyao's function: stateComputeAgent1()
+[agent1State_est_est, agent1Pos_est_est_Cov] = stateComputeAgent1(ekf,prev_pos_1, prev_vel_1, dt, curr_pos_1, drop_percent, est_est_flag);
+
+agent1Pos_est_est = agent1State_est_est(1:3,1);
+
+pos_meas_n = agent1Pos_est_est + rel_vec;
+
+meas_Cov_n = rel_Cov + agent1Pos_est_est_Cov; 
+            % Not sure about agent1Pos_est_est_Cov above, we can discuss on
+            % Saturday
